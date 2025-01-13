b2521a56a9c879e11ac20db688313d77bd1ecb65
URIAN VIERA <urian1213viera@gmail.com>
2025-01-13 15:38:21 -0500

    Update index.php

--- Backend-php/index.php
+++ Backend-php/index.php
@@ -8,7 +8,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require('configBD.php');
 $metodo = $_SERVER['REQUEST_METHOD'];
